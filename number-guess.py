3feff0d7176f28c00d881f9603bd25ad733026d1
IamAdv <104263903+IamAdv@users.noreply.github.com>
2022-12-06 17:32:21 +0530

    initial for number guessing thingy
    as the title says...

---
+++ number-guess.py
@@ -0,0 +1,9 @@
+print("This Program basically tries to guess Which Number you're thinkng of from the set of numbers given")
+print()
+
+#blah blah blah
+print("Think of any number from 1 to 63 and I'll try to guess it.")
+print() #some spaces to make it not look scuffed.
+input("Press [Enter] if you guessed the number ")
+print()
+
